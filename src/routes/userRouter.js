8cc0633d567e5a4c70ce5d8bc14df6533e69bcfb
VenantTumukunde <ventum11@gmail.com>
2023-04-02 18:09:29 +0200

    make a user an admin

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,6 +3,7 @@ import UserController from "../controllers/UserController";
 import NewUserDataChecker from "./../middlewares/NewUserDataChecker";
 import CheckPassword from "../middlewares/CheckPassword";
 import protectedRoute from "../middlewares/protectRoute";
+import isUserVerified from "../middlewares/isUserVerified";
 const router = express.Router();
 
 router.post(
@@ -23,6 +24,13 @@ router.post("/logout", UserController.logout);
 router.post("/verifyEmail", protectedRoute, UserController.verifyEmail);
 
 router.put("/profile/:id", UserController.updateUserProfile);
+
+router.put(
+  "/makeAdmin/:id",
+  protectedRoute,
+  isUserVerified,
+  UserController.makeUserAdmin
+);
 
 router.get("/profile/:id", UserController.getUserProfile);
 
