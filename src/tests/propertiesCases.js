db45dca74034cedbd37ec43009636abc1659cf5a
NGIRIMANA Schadrack <chadrackngirimana@gmail.com>
2023-04-18 09:56:45 +0200

    test errors (#38)
    * test errors
    
    * test data changes

--- src/tests/propertiesCases.js
+++ src/tests/propertiesCases.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/prefer-default-export */
 export const properties = [
   {
     title: "House for rent",
@@ -51,6 +52,96 @@ export const properties = [
     price: "600",
 
     upi: "12/03/45/678",
+    description: "a nice house",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      public_id: "hfdvjavshfvash",
+    },
+    otherImages: [
+      {
+        url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        public_id: "wrdsfvsf",
+      },
+      {
+        url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        public_id: "wrdsfdssf",
+      },
+    ],
+  },
+  {
+    title: "House for rent",
+    category: "house",
+    section: "renting",
+    price: "600",
+    size: "200",
+    upi: "12/03/45/678",
+
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      public_id: "hfdvjavshfvash",
+    },
+    otherImages: [
+      {
+        url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        public_id: "wrdsfvsf",
+      },
+      {
+        url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        public_id: "wrdsfdssf",
+      },
+    ],
+  },
+  {
+    title: "House for rent",
+    section: "renting",
+    price: "600",
+    size: "200",
+    upi: "12/03/45/678",
+    description: "a nice house",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      public_id: "hfdvjavshfvash",
+    },
+    otherImages: [
+      {
+        url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        public_id: "wrdsfvsf",
+      },
+      {
+        url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        public_id: "wrdsfdssf",
+      },
+    ],
+  },
+  {
+    title: "House for rent",
+    category: "house",
+    section: "renting",
+    size: "200",
+    upi: "12/03/45/678",
+    description: "a nice house",
+    mainImage: {
+      url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      public_id: "hfdvjavshfvash",
+    },
+    otherImages: [
+      {
+        url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        public_id: "wrdsfvsf",
+      },
+      {
+        url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        public_id: "wrdsfdssf",
+      },
+    ],
+  },
+  {
+    title: "House for rent",
+    category: "house",
+    section: "renting",
+    price: "600",
+    size: "200",
+
     description: "a nice house",
     mainImage: {
       url: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwaG91c2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
